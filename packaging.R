0196630c0f079219bedade44e722f7c9e75ffa73
mayer79 <mayermichael79@gmail.com>
2022-02-08 07:18:55 +0100

    reduced description file

--- packaging.R
+++ packaging.R
@@ -14,28 +14,25 @@ library(usethis)
 use_description(
   fields = list(
     Title = "Confidence Intervals",
-    Type = "Package",
-    Version = "0.1.2",
-    Date = Sys.Date(),
+    Version = "0.1.2.9000",
     Description = "Calculates classic and/or bootstrap confidence intervals for many parameters such as the population mean, variance, interquartile range (IQR), median absolute deviation (MAD), skewness, kurtosis, Cramer's V, odds ratio, R-squared, quantiles (incl. median), proportions, different types of correlation measures, difference in means, quantiles and medians. Many of the classic confidence intervals are described in Smithson, M. (2003, ISBN: 978-0761924999). Bootstrap confidence intervals are calculated with the R package 'boot'. Both one- and two-sided intervals are supported.",
     `Authors@R` = "person('Michael', 'Mayer', email = 'mayermichael79@gmail.com', role = c('aut', 'cre'))",
     Depends = "R (>= 3.1.0)",
-    LazyData = NULL,
-    Maintainer = "Michael Mayer <mayermichael79@gmail.com>"
+    LazyData = NULL
   ),
   roxygen = TRUE
 )
 
+use_package("stats", "Imports")
+use_package("boot", "Imports")
+
 use_gpl_license(2)
+
 use_github_links() # use this if this project is on github
 
 # Your files that do not belong to the package itself (others are added by "use_* function")
 use_build_ignore(c("^packaging.R$", "[.]Rproj$", "^backlog$",
                    "^cran-comments.md$", "^logo.png$"), escape = FALSE)
-
-# Required external packages
-use_package("stats", "Imports")
-use_package("boot", "Imports")
 
 # If your code uses the pipe operator %>%
 # use_pipe()

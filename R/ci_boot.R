c723edef7d2511179372fe486884fb498e66c6bd
Michael Mayer <mayermichael79@gmail.com>
2023-04-13 20:55:14 +0200

    remove ... from ci_boot()

--- R/ci_boot.R
+++ R/ci_boot.R
@@ -1,19 +1,16 @@
-#' Bootstrap Confidence Interval
+#' Bootstrap CI
 #'
-#' Internel wrapper to \code{boot::boot.ci}. Contains the logic for one-sided and asymmetric confidence intervals.
-#' @importFrom boot boot.ci
-#' @param S Result of \code{boot}.
-#' @param boot_type Confidence interval type passed to \code{boot::boot.ci}.
-#' @param probs Probability cuts for the confidence intervals.
-#' @param ... Further arguments passed to \code{boot::boot.ci}.
+#' Internal wrapper to \code{boot::boot.ci()}. Contains the logic for one-sided and asymmetric CIs.
+#' @param S Result of \code{boot::boot()}.
+#' @param boot_type CI type passed to \code{boot::boot.ci()}.
+#' @param probs Probability cuts for the CIs.
 #' @return A numeric vector of length two.
-ci_boot <- function(S, boot_type = c("norm", "basic", "stud", "perc", "bca"),
-                    probs, ...) {
+ci_boot <- function(S, boot_type = c("norm", "basic", "stud", "perc", "bca"), probs) {
   a <-  1 - 2 * probs[1L]
   b <- -1 + 2 * probs[2L]
   boot_type <- match.arg(boot_type)
   conf <- if (probs[1L] == 0) b else if (probs[2L] == 1) a else c(a, b)
-  cint <- boot.ci(S, conf = conf, type = boot_type, ...)[[map_boot_type(boot_type)]]
+  cint <- boot::boot.ci(S, conf = conf, type = boot_type)[[map_boot_type(boot_type)]]
   m <- ncol(cint)
   if (probs[1L] == 0) {
     return(c(-Inf, cint[1L, m]))
